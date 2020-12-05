cf5ae5d6b0a4cb2c7d7ee8aa31c66b87e91ff7ac
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2020-12-05 18:22:42 -0300

    Criando notificações por tópico.

--- notificacaoFirebase/app/src/main/java/com/daviprojetos/notificacaofirebase/MainActivity.java
+++ notificacaoFirebase/app/src/main/java/com/daviprojetos/notificacaofirebase/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /* Ativar tópicos(Exemplo : comidas)
+        ()Mexicana
+        ()Alemã
+        (X)Brasileira
+         */
+        /*No firebase na hora de enviar a notificação
+         selecionar a segmentação por tópico e selecionar o tópico desejado
+         Nesse caso o usuário está se inscrevendo para receber mensagens de comida brasileira*/
+
+        //Se inscrevendo no tópico de comida brasileira
+        FirebaseMessaging.getInstance().subscribeToTopic("brasileira");
+
+        //Desativando o tópico inscrito
+        //FirebaseMessaging.getInstance().unsubscribeFromTopic("brasileira");
+
     }
 }
